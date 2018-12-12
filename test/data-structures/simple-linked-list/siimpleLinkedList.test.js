7532b4301d365fb313d630255b1b0150b01ab6bd
twindyorg <twindyorg@163.com>
2018-12-12 21:50:46 +0800

    简单的单向链表单元测试

--- test/data-structures/simple-linked-list/siimpleLinkedList.test.js
+++ test/data-structures/simple-linked-list/siimpleLinkedList.test.js
@@ -1,33 +1,5 @@
 const SimpleLinkedList = require('../../../src/data-structures/simple-linked-list/simpleLinkedList');
 
-
-// const linkedList = new SimpleLinkedList();
-
-// linkedList.append('1');
-// linkedList.append('2');
-// linkedList.append('8');
-// linkedList.append('4');
-// linkedList.append('5');
-// linkedList.append('6');
-// linkedList.append('7');
-
-// linkedList.append("8")
-
-// console.log("------------------------->");
-// console.log(linkedList);
-// console.log("------------deleteHead------------->");
-// console.log(linkedList.deleteHead());
-// console.log("------------deleteHead------------->");
-// console.log(linkedList.deleteTail());
-// console.log("------------deleteTail------------->");
-
-// console.log("------------find------------->");
-// console.log(linkedList.find(4));
-// console.log("------------remove------------->");
-// console.log(linkedList.remove('1'));
-// console.log(linkedList);
-// console.log(linkedList.toString());
-
 test('01、创建一个空链表', ()=> {
     const linkedList = new SimpleLinkedList();
 
