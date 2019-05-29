c3d2742351f039bf6a6b5528dab39d1644915f72
twindyorg <twindyorg@163.com>
2019-05-29 11:42:45 +0800

    冒泡排序算法实现

--- src/algorithms/sort/bubble-sort/index.js
+++ src/algorithms/sort/bubble-sort/index.js
@@ -11,23 +11,24 @@ class BubbleSort {
      * @param {Array} originalArray 要进行排序的数组
      */
     static sort(originalArray) {
-        let changed = false;
+        // 是否排序完成的标志
+        let swapped = false;
         // 克隆一个新的数组
         const array = [...originalArray];
 
         for (let i = 0, _len = array.length; i < _len; i++) {
-            changed = false;
+            swapped = false;
             // 冒泡至少一个元素移动到它应该在的位置
             for (let j = 0; j < _len - i; j++) {
                 // 满足条件，置换位置
                 if (array[j] > array[j + 1]) {
                     [array[j], array[j + 1]] = [array[j + 1], array[j]];
-                    changed = true;
+                    swapped = true;
                 }
             }
 
             // 如果本次循环没有元素移动，则说明排序已经完成了，后面的操作可以不进行
-            if (!changed) {
+            if (!swapped) {
                 return array;
             }
         }
