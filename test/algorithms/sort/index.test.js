494a4111a544677379ac2d29f9cfac1c8230b7c6
twindyorg <twindyorg@163.com>
2019-05-30 19:37:42 +0800

    排序算法性能测试

--- test/algorithms/sort/index.test.js
+++ test/algorithms/sort/index.test.js
@@ -2,6 +2,7 @@ import SelectionSort from '../../../src/algorithms/sort/selection-sort';
 import InsertionSort from '../../../src/algorithms/sort/insertion-sort';
 import BubbleSort from '../../../src/algorithms/sort/bubble-sort';
 import MergeSort from '../../../src/algorithms/sort/merge-sort';
+import QuickSort from '../../../src/algorithms/sort/quick-sort';
 import Utils from '../../../src/utils';
 
 /**
@@ -9,6 +10,12 @@ import Utils from '../../../src/utils';
  */
 function sortCheck(originalArray) {
     const result = {};
+
+    // 快速排序
+    result.QuickSort = {};
+    result.QuickSort.beignTime = Date.now();
+    QuickSort.sort(originalArray);
+    result.QuickSort.endTime = Date.now();
 
     // 冒泡排序
     result.BubbleSort = {};
@@ -22,17 +29,17 @@ function sortCheck(originalArray) {
     InsertionSort.sort(originalArray);
     result.InsertionSort.endTime = Date.now();
 
-    // 选择排序
-    result.SelectionSort = {};
-    result.SelectionSort.beignTime = Date.now();
-    SelectionSort.sort(originalArray);
-    result.SelectionSort.endTime = Date.now();
-
     // 归并排序
     result.MergeSort = {};
     result.MergeSort.beignTime = Date.now();
     MergeSort.sort(originalArray);
     result.MergeSort.endTime = Date.now();
+
+    // 选择排序
+    result.SelectionSort = {};
+    result.SelectionSort.beignTime = Date.now();
+    // SelectionSort.sort(originalArray);
+    result.SelectionSort.endTime = Date.now();
 
     return result;
 }
@@ -43,8 +50,8 @@ function sortCheck(originalArray) {
 
 test('性能测试', ()=> {
     // 随机生成长度为1000，值在[0, 10000]之间的数组
-    const array = Array.from({length: 1000}, ()=> {
-        return Utils.getRandomNum(0, 105000);
+    const array = Array.from({length: 500}, ()=> {
+        return Utils.getRandomNum(0, 1050000);
     });
 
     const result = sortCheck(array);
@@ -53,6 +60,7 @@ test('性能测试', ()=> {
     const InsertionSort1 = result.InsertionSort.endTime - result.InsertionSort.beignTime;
     const SelectionSort1 = result.SelectionSort.endTime - result.SelectionSort.beignTime;
     const MergeSort1 = result.MergeSort.endTime - result.MergeSort.beignTime;
+    const QuickSort1 = result.QuickSort.endTime - result.QuickSort.beignTime;
 
     const result2 = sortCheck(array);
 
@@ -60,11 +68,13 @@ test('性能测试', ()=> {
     const InsertionSort2 = result2.InsertionSort.endTime - result2.InsertionSort.beignTime;
     const SelectionSort2 = result2.SelectionSort.endTime - result2.SelectionSort.beignTime;
     const MergeSort2 = result2.MergeSort.endTime - result2.MergeSort.beignTime;
+    const QuickSort2 = result2.QuickSort.endTime - result2.QuickSort.beignTime;
 
     // eslint-disable-next-line
     console.log(`针对长度为100的数组,使用各算法耗时统计如下：
 冒泡排序算法: 第一次：${BubbleSort1}ms 第二次：${BubbleSort2}ms 平均：${(BubbleSort1 + BubbleSort2) / 2}ms
 插入排序算法: 第一次：${InsertionSort1}ms 第二次：${InsertionSort2}ms 平均：${(InsertionSort1 + InsertionSort2) / 2}ms
 选择排序算法: 第一次：${SelectionSort1}ms 第二次：${SelectionSort2}ms 平均：${(SelectionSort1 + SelectionSort2) / 2}ms
-归并排序算法: 第一次：${MergeSort1}ms 第二次：${MergeSort2}ms 平均：${(MergeSort1 + MergeSort2) / 2}ms`);
+归并排序算法: 第一次：${MergeSort1}ms 第二次：${MergeSort2}ms 平均：${(MergeSort1 + MergeSort2) / 2}ms
+快速排序算法: 第一次：${QuickSort1}ms 第二次：${QuickSort2}ms 平均：${(QuickSort1 + QuickSort2) / 2}ms`);
 });
