8d8de22a8bbe88331cacefd9725e7293784f2f77
twindyorg <twindyorg@163.com>
2020-06-24 11:30:08 +0800

    计数二进制

--- src/leetcode/string/count-binary-substrings.js
+++ src/leetcode/string/count-binary-substrings.js
@@ -2,21 +2,29 @@
  * 计数二进制
  * https://leetcode-cn.com/problems/count-binary-substrings/
  */
-const cbs1 = str=> {
+
+/**
+ * 方案一
+ * @param {*} str
+ */
+const countBinarySubstrings1 = str=> {
 
     /**
      * 返回符合条件的字串，每次只会返回一条
      * @param {*} String
      */
-    function getMatchedString(String) {
-        const strArray = String.split('');
+    function getMatchedString(s) {
+        const strArray = s.split('');
 
-        let leftIndex = 1, rightIndex = 0, len = strArray.length - 1, result = strArray[0];
+        let leftIndex = 1, rightIndex = 0, len = strArray.length, result = strArray[0];
 
         for (let i = 1; i < len; i++) {
             if (strArray[i] !== strArray[0]) {
                 if (rightIndex === 0) {
-                    len = 2 * leftIndex;
+                    const _len = 2 * leftIndex;
+
+                    // 确保新的len不超过数组长度
+                    len = _len < len ? _len : len;
                 }
                 rightIndex++;
             } else {
@@ -44,6 +52,64 @@ const cbs1 = str=> {
     return result.length;
 };
 
+
+
+/**
+ * 方案二, 利用正则
+ * @param {*} str
+ */
+const countBinarySubstrings2 = str=> {
+    // 建立数据结构，堆栈，保存数据
+    const r = [];
+    // 给定任意子输入都返回第一个符合条件的子串
+    const getMatchedString = str=> {
+        const j = str.match(/^(0+|1+)/)[0];
+        const o = (j[0] ^ 1).toString().repeat(j.length);
+        const reg = new RegExp(`^(${j}${o})`);
+
+        if (reg.test(str)) {
+            return RegExp.$1;
+        } else {
+            return '';
+        }
+    };
+
+    // 通过for循环控制程序运行的流程
+    for (let i = 0, len = str.length - 1; i < len; i++) {
+        const sub = getMatchedString(str.slice(i));
+
+        if (sub) {
+            r.push(sub);
+        }
+    }
+    return r.length;
+};
+
+/**
+ * 方案三
+ * @param {*} str
+ */
+const countBinarySubstrings3 = str=> {
+    let last = 0, // 记录之前一种数字的个数
+        res = 0, // 符合期望的个数
+        cur = 1; // 记录当前数字的个数
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i] === str[i - 1]) {
+            cur++;
+        } else {
+            last = cur;
+            cur = 1;
+        }
+        if (last >= cur) {
+            res++;
+        }
+    }
+    return res;
+};
+
 export {
-    cbs1
+    countBinarySubstrings1,
+    countBinarySubstrings2,
+    countBinarySubstrings3
 };
--- test/leetcode/string/count-binary-substrings.test.js
+++ test/leetcode/string/count-binary-substrings.test.js
@@ -1,25 +1,45 @@
-import {cbs1} from '../../../src/leetcode/string/count-binary-substrings';
+import {countBinarySubstrings1, countBinarySubstrings2, countBinarySubstrings3} from '../../../src/leetcode/string/count-binary-substrings';
 import Utils from '../../../src/utils';
 
 test('01、计数二进制', ()=> {
-    expect(cbs1('00110011')).toEqual(6);
+    expect(countBinarySubstrings1('00110011')).toEqual(6);
+    expect(countBinarySubstrings2('00110011')).toEqual(6);
+    expect(countBinarySubstrings3('00110011')).toEqual(6);
+    expect(countBinarySubstrings1('00110')).toEqual(3);
+    expect(countBinarySubstrings2('00110')).toEqual(3);
+    expect(countBinarySubstrings3('00110')).toEqual(3);
 });
 
 test('01、计数二进制性能测试', ()=> {
 
-    // 随机生成长度为5000，值在[0, 1]之间的数组
-    const binaryString = Array.from({length: 5000}, ()=> {
+    // 随机生成长度为10000，值在[0, 1]之间的数组
+    const binaryString = Array.from({length: 10000}, ()=> {
         return Utils.getRandomNum(0, 1);
     }).join('');
 
-    const startTime = Date.now();
+    const perf = {};
 
-    cbs1(binaryString);
+    perf.cbs1 = {};
+    perf.cbs2 = {};
+    perf.cbs3 = {};
 
-    const endTime = Date.now();
+    perf.cbs1.startTime = Date.now();
+    countBinarySubstrings1(binaryString);
+    perf.cbs1.endTime = Date.now();
 
-    console.log(`针对长度为5000的二进制字符串，共耗时${endTime - startTime}ms`);
+    perf.cbs2.startTime = Date.now();
+    countBinarySubstrings2(binaryString);
+    perf.cbs2.endTime = Date.now();
 
+    perf.cbs3.startTime = Date.now();
+    countBinarySubstrings2(binaryString);
+    perf.cbs3.endTime = Date.now();
+
+    // eslint-disable-next-line
+    console.log(`针对长度为10000的二进制字符串，
+方案一：共耗时${perf.cbs1.endTime - perf.cbs1.startTime}ms
+方案二：共耗时${perf.cbs2.endTime - perf.cbs2.startTime}ms
+方案三：共耗时${perf.cbs3.endTime - perf.cbs3.startTime}ms`);
 });
 
 
